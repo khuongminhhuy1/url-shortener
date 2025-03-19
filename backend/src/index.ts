41d984da69a68480a0d0b1d2ea18b4b2ede2a28b
Minh Huy <khuongminhhuy1505@gmail.com>
2025-03-19 17:56:07 +0700

    testing domain

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.APP_URL,
+    origin: [process.env.APP_URL || "http://localhost:3000", "https://url-shortener-8hr.pages.dev"],
     credentials: true,
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
