0148ac4d333d855844e74a4f8cf22f92c553a592
Minh Huy <khuongminhhuy1505@gmail.com>
2025-03-19 18:11:17 +0700

    testing domain

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,14 +12,7 @@ dotenv.config(); // Use dotenv
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [process.env.APP_URL || "http://localhost:3000", "https://url-shortener-8hr.pages.dev"],
-    credentials: true,
-    methods: "GET, POST, PUT, DELETE, OPTIONS",
-    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
-  })
-);
+app.use(cors());
 app.options("*", cors());
 
 app.use("/api/url", urlRoute); // Use urlRoute
