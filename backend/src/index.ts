b3efb9a3b941c411bb8d9f577197f3189d766d29
Minh Huy <khuongminhhuy1505@gmail.com>
2025-03-19 19:30:49 +0700

    production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: app_url,
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
